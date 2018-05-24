6e230a5beae8cda56c54c12d278d7dc30ea8650e
ProjetoPoli <39572735+ProjetoPoli@users.noreply.github.com>
2018-05-24 12:12:25 -0300

    Update Lorde.java

--- Lorde.java
+++ Lorde.java
@@ -1,7 +1,48 @@
 package starWars;
 
 public class Lorde extends Sith {
+	
+	private int pontosDeVida;
+	private int dominioDaForca;
+	private int dominioDeSabre;
+	private Habilidade[] habilidades = new Habilidade[4];
 
+	public int getPontosDeVida() {
+		return pontosDeVida;
+	}
+
+	public void setPontosDeVida(int pontosDeVida) {
+		this.pontosDeVida = pontosDeVida;
+	}
+
+	public int getDominioDaForca() {
+		return dominioDaForca;
+	}
+
+	public void setDominioDaForca(int dominioDaForca) {
+		this.dominioDaForca = dominioDaForca;
+	}
+
+	public int getDominioDeSabre() {
+		return dominioDeSabre;
+	}
+
+	public void setDominioDeSabre(int dominioDeSabre) {
+		this.dominioDeSabre = dominioDeSabre;
+	}
+
+	public Habilidade[] getHabilidades() {
+		return habilidades;
+	}
+
+	public void setHabilidades(Habilidade[] habilidades) {
+		this.habilidades = habilidades;
+	}
+	
+	public void descricao() {
+		System.out.println("Um Lorde Sith eh um Sith que passou por intenso treinamento e foi reconhecido por outros lordes.");
+	}
+	
 	void InfosPessoais() {
 		System.out.println("Esse eh o Lorde Sith " + nome + ", tambem conhecido por " + nomeSith
 				+ ", que nasceu em " + localDeNascimento + ", no ano " + anoDeNascimento + ' ' + epocaDeNascimento +'.');
