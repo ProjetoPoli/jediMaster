f0132f91426d5952a1884b4275904af53f4d8fae
ProjetoPoli <39572735+ProjetoPoli@users.noreply.github.com>
2018-05-24 10:33:47 -0300

    Update Youngling.java

--- Youngling.java
+++ Youngling.java
@@ -4,11 +4,14 @@ public class Youngling extends Jedi{
 
 	String treinador;
 	
-	public Youngling(int data, String nom, String sex, String esp, String epoch, String loc, String trei) {
-		super(data, nom, sex, esp, epoch, loc);
-		treinador = trei;
+	public String getTreinador() {
+		return treinador;
 	}
-	
+
+	public void setTreinador(String treinador) {
+		this.treinador = treinador;
+	}
+
 	public void descricao() {
 		System.out.println(" O Youngling ou iniciado na ordem Jedi era um ser crianca designada para treinamento em algum Cla, sempre treinado por um mestre da ordem Jedi. "
 				+ "Um marco importante desse estagio era se o Youngling ja tinha feito a colheita de cristais para seus sabres de luz.");
